7ca55318355ded99d34c6ad65d681419de037a29
zbeucler2018 <zbeucler@gmail.com>
2021-02-01 19:35:21 -0500

    change background and name title

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -60,7 +60,7 @@ class Header extends Component {
 
         <div className="row banner">
           <div className="banner-text">
-            <h1 className="responsive-headline">{name}</h1>
+            <h1 className="responsive-headline">Hi, I'm Zack Beucler</h1>
             <h3>
               I'm a {city} based <span>{occupation}</span>. {description}.
             </h3>
