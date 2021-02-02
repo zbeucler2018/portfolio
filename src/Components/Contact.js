6e070db90386952a6da34b8583e5d2419ca06365
zbeucler2018 <49871927+zbeucler2018@users.noreply.github.com>
2021-02-02 13:05:21 -0500

    remove .env console log

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -37,13 +37,6 @@ class Contact extends Component {
             );
             console.log(error.text);
             clearContactForm();
-            console.log(
-              process.env.REACT_APP_EMAILJS_SERVICE_ID +
-                " " +
-                process.env.REACT_APP_EMAILJS_TEMPLATE_ID +
-                " " +
-                process.env.REACT_APP_EMAILJS_USER_ID
-            );
           }
         );
     }
